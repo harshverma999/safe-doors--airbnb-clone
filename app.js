5c316f8604fa9ff5a73bfca966c37fe3b35a1b21
Harsh Kumar Verma <romanharsh2@gmail.com>
2026-08-29 18:47:50 +0530

    Add listing CRUD routes and EJS views

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/safedoor"
 const Listing = require("./models/listing.js");
-
+const path = require("path");
+const methodOverride = require("method-override");
 
 main()
 .then(()=>{
@@ -14,20 +15,91 @@ main()
 })
 
 async function main(){
-await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL);
 };
 
-app.get("/sampleListing",async(req, res)=>{
-    let testListing = new Listing({
-        title:"Fully air",
-        description:"aajao",
-        price:1000,
-        location:"palamu",
-        country:"India",
-    });
-    await testListing.save();
-    res.send("test passed");
-});
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"))
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"));
+
+// show all linsting
+app.get("/listings",async (req,res)=>{
+    const allListing = await Listing.find({}).catch((err)=>console.log(err))
+    res.render("listings/index.ejs",{allListing})
+})
+
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
+// app.post("/listings",async (req,res)=>{
+//     const {title,description,price,location,country} = req.body;
+
+//     await Listing.insertOne({
+//         title: title,
+//         description: description,
+//         price: price,
+//         location: location,
+//         country: country,
+//     });
+//     res.redirect("/listings")
+
+// })
+
+// Create route
+app.post("/listings",async (req,res)=>{
+    // const listing = req.body.listing
+    // console.log(listing)
+
+    const listing = new Listing(req.body.listing)
+    await listing.save()
+    res.redirect("/listings")
+})
+app.delete("/listings/:id",async (req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndDelete(id)
+    res.redirect("/listings")
+})
+
+// update route
+app.put("/listings/:id",async (req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.prevData})
+    res.redirect(`/listings/${id}`)
+})
+
+// Edit route
+app.get("/listings/:id/edit",async (req, res)=>{
+    const {id} = req.params;
+
+    const prevData = await Listing.findById(id)
+    res.render("listings/edit.ejs",{prevData})
+})
+
+// show individual list
+app.get("/listings/:id", async(req,res)=>{
+    let {id} = req.params;
+    const indvList = await Listing.findById(id).catch((err)=>{console.log(err)})
+    res.render("listings/show.ejs",{indvList})
+})
+
+
+// data init testing...
+// app.get("/sampleListing",async(req, res)=>{
+//     let testListing = new Listing({
+//         title:"Fully air",
+//         description:"aajao",
+//         price:1000,
+//         location:"palamu",
+//         country:"India",
+//     });
+//     await testListing.save();
+//     res.send("test passed");
+// });
+
+
+
 
 app.get("/",(req,res)=>{
     res.send("Root is Working");
