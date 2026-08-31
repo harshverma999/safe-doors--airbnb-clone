1237cf5ad1050d60f8ba1f52c21681847b62269f
Harsh Kumar Verma <romanharsh2@gmail.com>
2026-08-31 18:56:03 +0530

    Add Bootstrap styling and layouts

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/safedoor"
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 main()
 .then(()=>{
@@ -19,9 +20,11 @@ async function main(){
 };
 
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"))
-app.use(express.urlencoded({extended:true}))
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 // show all linsting
 app.get("/listings",async (req,res)=>{
